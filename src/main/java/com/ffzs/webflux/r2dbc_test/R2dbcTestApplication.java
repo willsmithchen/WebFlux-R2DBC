5f38f1f18b3283b3123b43bd662f441197245311
ffzs <dz759043338@163.com>
2020-08-10 17:59:50 +0800

    webflux+r2dbc+springdoc

---
+++ src/main/java/com/ffzs/webflux/r2dbc_test/R2dbcTestApplication.java
@@ -0,0 +1,13 @@
+package com.ffzs.webflux.r2dbc_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class R2dbcTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(R2dbcTestApplication.class, args);
+    }
+}
